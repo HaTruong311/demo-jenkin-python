04421f81cbecc9263a61d1feb551673b0d1669de
Eddy <daokythanh@gmail.com>
2025-05-16 23:47:38 +0700

    change port to access from host machine

--- app.py
+++ app.py
@@ -11,4 +11,6 @@ def hello_world():  # put application's code here
     return 'Hello World!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Bind to 0.0.0.0 to allow external access
+    # Use port 5000 to be consistent with Jenkinsfile
+    app.run(host='0.0.0.0', port=5000, debug=False)
